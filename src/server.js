070ca36c3f7f486ae2cc6c1d12afaf622678f21c
phong2112004-oss <phong2112004@gmail.com>
2026-07-02 17:12:55 +0700

    firebase deploy

--- src/server.js
+++ src/server.js
@@ -32,7 +32,9 @@ if (!admin.apps.length) {
 
 app.use('/api', dashboardRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server is running locally on port ${PORT}`);
+  });
+}
